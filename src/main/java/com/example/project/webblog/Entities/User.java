2a1a1d7259636ba69dde95cb0b0288d065fb8ea8
Daniel Kovacs <kdaniel202@gmail.com>
2022-11-14 12:44:46 +0100

    HTML pages added!

--- src/main/java/com/example/project/webblog/Entities/User.java
+++ src/main/java/com/example/project/webblog/Entities/User.java
@@ -1,7 +1,19 @@
 package com.example.project.webblog.Entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
